241e9e398e6549eba3a25480bad8fa83f9699fde
mjmandelah29 <mojisolagracearamide7@gmail.com>
2022-12-08 13:22:42 -0800

    size of c types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * Description: main - Entry point
+ *
+ * Return:  Always 0 (success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+ 
+printf("Size of a char: %ld byte(s)\n", sizeof(a));
+printf("Size of an int: %ld byte(s)\n", sizeof(b));
+printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
+printf("Size of a float: %ld byte(s)\n", sizeof(e));
+return (0);
+}
+
+
+
+
+
+
+
