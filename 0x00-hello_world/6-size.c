9536e7796ac1d88f8e7195285b075009d94af50a
mjmandelah29 <mojisolagracearamide7@gmail.com>
2022-12-08 13:25:47 -0800

    size of c types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ int b;
 long int c;
 long long int d;
 float e;
- 
 printf("Size of a char: %ld byte(s)\n", sizeof(a));
 printf("Size of an int: %ld byte(s)\n", sizeof(b));
 printf("Size of a long int: %ld byte(s)\n", sizeof(c));
@@ -20,6 +19,7 @@ printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
 printf("Size of a float: %ld byte(s)\n", sizeof(e));
 return (0);
 }
+
 
 
 
