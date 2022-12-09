43fa90975a8709454368afc2da5920b020cc169f
mjmandelah29 <mojisolagracearamide7@gmail.com>
2022-12-09 12:30:11 -0800

    Print all numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print all singke digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+for (digit = 0; digit < 10; digit++)
+printf("%i", digit);
+putchar('\n');
+return (0);
+}
